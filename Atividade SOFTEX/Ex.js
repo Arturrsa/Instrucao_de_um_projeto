49ea9858662b8202f0f9a4e7495102f0f12f78c6
artur henrique <arturr.sanntos@gmail.com>
2022-09-06 10:23:44 -0300

    Create Ex.js

---
+++ Atividade SOFTEX/Ex.js
@@ -0,0 +1,13 @@
+/*Acesse pelo menos dois sites de sua preferência e os inspecione
+com o botão direito do mouse na página web.
+Com o código fonte aberto você deve 
+Verificar no código algum elemento que utilize JavaScript; 
+Marcar alguns elementos do site; 
+Explicar como ele se comporta. Exemplo: entrar no site do Google,
+ inspecionar o botão de pesquisa, verificar o código e explicar qual a finalidade do botão.*/ 
+
+
+ <span class="mosaico__header-personalization--times-label" style="display: block;">TIMES</span>
+ /* ao entrar no site do globoesporte, inspecionei o botão time, a finalidade do botão 
+ me trouxe várias opções de time para serem escolhidos.*/
+ 
